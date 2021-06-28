d27bbecbda6967c32b489817300eb9c6d3614aaa
yoonhero <yoonhero06@naver.com>
2021-06-28 16:53:43 +0900

    6/28

---
+++ app.js
@@ -0,0 +1,25 @@
+const countDate = new Date("June 30, 2021 09:30:00").getTime();
+
+function newYear() {
+  let now = new Date().getTime();
+  let gap = countDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+  document.querySelector(".seconds").innerText = `${Math.floor(gap / 1000)}초`;
+}
+
+setInterval(function () {
+  newYear();
+}, 1000);
